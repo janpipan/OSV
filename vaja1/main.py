e465ac6ba9d78e6c63b39a74b83aefdadb08eb34
Jan Pipan <jpipan@nil.com>
2022-10-15 18:32:55 +0200

    first commit

---
+++ vaja1/main.py
@@ -0,0 +1,71 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+"""
+image = open(raw_img, 'rb')
+buffer = image.read()
+image.close()
+
+s1 = np.ndarray((512,410), dtype=np.uint8, buffer=buffer, order='F')
+
+plt.figure()
+plt.imshow(s1, cmap='gray') """
+
+# Naloga 2
+def loadImage(iPath, iSize, iType):
+    fid = open(iPath, 'rb')
+    buffer = fid.read()
+    fid.close()
+
+    len_buffer = len(np.frombuffer(buffer=buffer, dtype=iType))
+
+    if len_buffer != np.prod(iSize):
+        oShape = (iSize[1], iSize[0], 3)
+    else:
+        oShape = (iSize[1], iSize[0])
+    oImage = np.ndarray(oShape, dtype=iType, buffer=buffer, order='F')
+    return oImage
+
+
+def displayImage(iImage, iTitle):
+    plt.figure()
+    plt.title(iTitle)
+    plt.imshow(iImage, cmap='gray',aspect='equal')
+
+
+
+# funkcija ki piše v raw format
+def saveImage(iImage, iPath, iType):
+    pass
+    
+if __name__ == '__main__':
+    picture = plt.imread("./vaja1/data/lena-color.png")
+
+    # Naloga 1
+    print(picture.shape)
+
+    #plt.figure()
+
+    #plt.imshow(picture)
+
+    #plt.imsave('./data/lena.jpeg', picture)
+
+
+    raw_img = './vaja1/data/lena-gray-410x512-08bit.raw'
+
+    lena_gray= loadImage(raw_img, (410,512), np.uint8)
+
+    #plt.figure()
+    #plt.imshow(lena_grey, cmap='gray')
+
+    lena_color_path = './vaja1/data/lena-color-512x410-08bit.raw'
+
+    lena_color = loadImage(lena_color_path, (512,410), np.uint8)
+
+    plt.imshow(lena_color)
+
+    displayImage(lena_color, 'Lena color')
+
+    displayImage(lena_gray, 'Lena gray')
+
---
+++ vaja2/skripta2.py
@@ -0,0 +1,77 @@
+from matplotlib import pyplot as plt
+import numpy as np
+from vaja1.main import displayImage, loadImage
+
+
+
+
+def computeHistogram(iImage):
+    # histogram
+    """ oHist = [0] * 255
+    oProb = 0
+    oLevels =0
+    oCDF = 0
+    
+    for i in range(len(iImage)):
+        for j in range(len(iImage[i])):
+           oHist[iImage[i][j]] += 1  """
+    # število bitov uporabljenih za zapis intenzitete bita
+    nBits = int(np.log2(iImage.max())) + 1
+
+    oLevels = np.arange(0, 2**nBits, 1)
+
+    oHist = np.zeros(len(oLevels))
+    
+
+    for y in range(iImage.shape[0]):
+        for x in range(iImage.shape[1]):
+            oHist[iImage[y][x]] += 1
+
+    oProb = oHist / iImage.size
+    
+    oCDF = np.zeros_like(oHist, dtype=float)
+    for i in range(len(oProb)):
+        oCDF[i] = oProb[:i].sum() 
+
+    return oHist, oProb, oCDF, oLevels
+
+def equalizeHistogram(iImage):
+    h, p, cdf, l = computeHistogram(iImage)
+    nBits = int(np.log2(iImage.max())) + 1
+    smax = 2**nBits - 1
+    # T = np.floor(cdf*smax)
+    oImage = np.zeros_like(iImage)
+
+    for y in range(iImage.shape[0]):
+        for x in range(iImage.shape[1]):
+            s_i = iImage[y][x]
+            oImage[y][x] = int(cdf[s_i] * smax)
+            #oImage[y][x] = T[iImage[y,x]]
+
+    return oImage
+
+def displayHistogram(iHist, iLevels, iTitle):
+    plt.figure()
+    plt.title(iTitle)
+    plt.bar(iLevels, iHist, width=1, edgecolor='darkred', color='red')
+    plt.xlim((iLevels.min(), iLevels.max()))
+    plt.ylim((0, 1.05*iHist.max()))
+    plt.show()
+
+    
+
+if __name__ == '__main__':
+    image = loadImage("./vaja2/data/valley-1024x683-08bit.raw", (1024,683), np.uint8)
+    h, p, cdf ,l = computeHistogram(image)
+    displayHistogram(h,l,'Histogram')
+    displayHistogram(p,l, 'Prob')
+    displayHistogram(cdf, l, 'cdf')
+    displayImage(image,'original')
+    izravnan = equalizeHistogram(image)
+    h1, p1, cdf1, l1 = computeHistogram(izravnan)
+    displayHistogram(h1,l1,'Histogram izravnan')
+    displayHistogram(p1,l1, 'Prob izravanan')
+    displayHistogram(cdf1, l1, 'cdf izravnan')
+    displayImage(equalizeHistogram(image), 'izravnan historgram')
+    #print(histogram)
+    #displayHistogram(histogram[0], histogram[], 'histogram')
