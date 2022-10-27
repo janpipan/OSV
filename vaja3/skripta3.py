ad6794fe0cbb70e90c4de79dd91fe0043fc27ffa
Jan <janpipan@gmail.com>
2022-10-27 23:35:36 +0200

    koncana vaja 3

--- vaja3/skripta3.py
+++ vaja3/skripta3.py
@@ -48,81 +48,118 @@ def interpolateImage(iImage, iSize, iOrder):
 
 
 
-def displayImage(iImage, iTitle, iGridX, iGridY):
+def displayImage(iImage, iTitle, iGridX=None, iGridY=None):
     plt.figure()
     plt.title(iTitle)
-    plt.imshow(iImage, cmap='gray',aspect='equal',extent=(iGridX[0], iGridX[1], iGridY[0], iGridY[1]))
+    if iGridX != None or iGridY != None:
+        plt.imshow(iImage, cmap='gray',aspect='equal',extent=(iGridX[0], iGridX[1], iGridY[0], iGridY[1]))
+    else:
+        plt.imshow(iImage, cmap='gray',aspect='equal')
 
+""" def getMinMaxAverage(p):
+    minimum, maximum, average = -1, 0, 0  
+    for i in range(len(p)):
+        average += p[i] * i
+        if p[i] > 0 and minimum == -1:
+            minimum = i
+        elif p[i] > 0:
+            maximum = i
+    return minimum, maximum, average  """
 
 if __name__ == "__main__":
     orig_size = [200,152]
     image = loadImage('./vaja3/data/pumpkin-200x152-08bit.raw',orig_size,np.uint8)
 
-    displayImage(image,"Pumpkin",(0,200),(0,152))
+    #displayImage(image,"Pumpkin")
 
-    interpolateZero = interpolateImage(image,2*np.array(orig_size), 0)
+    #interpolateZero = interpolateImage(image,2*np.array(orig_size), 0)
 
-    displayImage(interpolateZero, "Pumpkin interpolate zero",(0,400),(0,304))
+    #displayImage(interpolateZero, "Pumpkin interpolate zero")
 
-    interpolateFirst = interpolateImage(image,2*np.array(orig_size), 1)
+    #interpolateFirst = interpolateImage(image,2*np.array(orig_size), 1)
 
-    displayImage(interpolateFirst, "Pumpkin iterpolate first",(0,400),(0,304))
+    #displayImage(interpolateFirst, "Pumpkin iterpolate first")
 
     # 1. Naloga
-    # Določite interpolacijsko sliko kot območje velikosti X x Y = 65 x 50 slikovnih elementov v dani sliki, pri čemer se prvi slikovni element območja nahaja 
-    # na lokaciji (x,y) = (75,30) slikovnih elementov v dani sliki (to je v 75-stolpcu in 30-ti vrstici slike).
-    # Priložite sliko dobljenega interpolacijskega območja. Priložite tudi sliko histograma interpolacijskega območja ter zapišite minimalno, maksimalno in povprečno 
+    # Določite interpolacijsko sliko kot območje velikosti X x Y = 65 x 50 slikovnih elementov v 
+    # dani sliki, pri čemer se prvi slikovni element območja nahaja 
+    # na lokaciji (x,y) = (75,30) slikovnih elementov v dani sliki (to je v 75-stolpcu in 30-ti 
+    # vrstici slike).
+    # Priložite sliko dobljenega interpolacijskega območja. Priložite tudi sliko histograma 
+    # interpolacijskega območja ter zapišite minimalno, maksimalno in povprečno 
     # sivinsko vrednost območja.
 
+    # slika, ki bo interpolirana
     image_interpolate = np.zeros((50,65),dtype=int)
+    # začetne vrednosti v originalni sliki
     start_y, start_x = 29,74
     for y in range(image_interpolate.shape[0]):
         for x in range(image_interpolate.shape[1]):
             image_interpolate[y][x] = image[start_y+y][start_x+x]
     
-    displayImage(image_interpolate, "Interpolacijska slika",(0,65),(0,50))
+    displayImage(image_interpolate, "Interpolacijska slika")
 
     h, p, cdf, l = computeHistogram(image_interpolate)
 
     displayHistogram(h, l, "Histogram interpolacijske slike")
-    
+
+    saveImage(image_interpolate, './vaja3/data/image.raw',np.uint8)
+
+    print(f"Min: {image_interpolate.min()}, Max: {image_interpolate.max()}, Povprečna: {np.average(image_interpolate)}")
 
 
     # 2. Naloga
-    # Priložite sliko interpoliranega območja velikosti X x Y = 600 x 300 slikovnih elemenotov, pridobljenega z interpolacijo ničtega reda interpolacijskega območja. Priložite tudi
-    # sliko histograma interpoliranega območja ter zapišite minimalno, maksimalno in povprečno sivinsko vrednost interpoliranega območja.
+    # Priložite sliko interpoliranega območja velikosti X x Y = 600 x 300 slikovnih elemenotov, 
+    # pridobljenega z interpolacijo ničtega reda interpolacijskega območja. Priložite tudi
+    # sliko histograma interpoliranega območja ter zapišite minimalno, maksimalno in povprečno 
+    # sivinsko vrednost interpoliranega območja.
 
     new_size = (600,300)
     image_interpolate_zero = interpolateImage(image_interpolate,new_size,0)
 
-    displayImage(image_interpolate_zero, "Interpolacija ničtega reda",(0,600),(0,300))
+    displayImage(image_interpolate_zero, "Interpolacija ničtega reda")
 
     h0, p0, cdf0, l0 = computeHistogram(image_interpolate_zero)
 
     displayHistogram(h0, l0, "Histogram interpolacije ničtega reda")
 
+    print(f"Min: {image_interpolate_zero.min()}, Max: {image_interpolate_zero.max()}, Povprečna: {np.average(image_interpolate_zero)}")
+    print("Kaj so prednosti in kaj slabosti interpolacije ničtega reda?")
+    print("Prednosti interpolacije ničtega reda so enostavna implementacija, hkrati pa se tudi ne spreminjajo vrednosti. Slabost interpolacije ničtega reda je, da so robovi bolj grobi.")
     # 3. Naloga
     # Kaj so prednosti in slabosti interpolacije prvega reda? 
-    print("Robovi na sliki so bolj gladki, vendar pa je celotna slika bolj zamegljena")
-    # Priložite sliko interpoliranega območja velikosti X x Y = 600 x 300 slikovnih elemenotv, pridobljenega z interpolacijo prvega reda interpolacijskega območja. Priložite tudi 
-    # sliko histograma interpoliranega območja ter zapišite minimalno, maksimalno in povprečno sivinsko vrednost interpoliranega območja.
+    
+    # Priložite sliko interpoliranega območja velikosti X x Y = 600 x 300 slikovnih elemenotv, 
+    # pridobljenega z interpolacijo prvega reda interpolacijskega območja. Priložite tudi 
+    # sliko histograma interpoliranega območja ter zapišite minimalno, maksimalno in povprečno 
+    # sivinsko vrednost interpoliranega območja.
 
     image_interpolate_one = interpolateImage(image_interpolate,new_size,1)
 
-    displayImage(image_interpolate_one, "Interpolacija prvega reda",(0,600),(0,300))
+    displayImage(image_interpolate_one, "Interpolacija prvega reda")
 
     h1, p1, cdf1, l1 = computeHistogram(image_interpolate_one)
 
     displayHistogram(h1, l1, "Histogram interpolacije prvega reda")
 
-
+    print(f"Min: {image_interpolate_one.min()}, Max: {image_interpolate_one.max()}, Povprečna: {np.average(image_interpolate_one)}")
+    print("Kaj so prednosti in kaj slabosti interpolacije prvega reda?")
+    print("Robovi na sliki so bolj gladki, vendar pa je celotna slika bolj zamegljena. Bolj zapletena je tudi njena implemntacija.")
+    
     # 4. Naloga
     # Kaj dosežemo z interpolacijami višjih redov, npr. z interpolacijo tretjega reda?
+    print("Z interpolacijami višjega reda slika postane bolj meglena v primerjavi z interpolacijami nižjega reda, vendar pa tudi robovi postanejo bolj gladki (ne izgledajo tako pixlasto).")
 
     # 5. Naloga
-    # Pri prikazovanju slik vzpostavite dimenzijsko sorazmerje med interpolacijskim in interpoliranim območjem (tj. območji morata biti enakih fizičnih dimenzij), in sicer tako, 
-    # da spremenite obstoječo funkcijo displayImage z dodatnima vhodnima argumentom iGridX in iGridY, ki predstavljata vektorja položajev slikovnih elementov v x in y smeri, ki
-    # omogočata izračun parametra extent v funkciji imshow iz modula matplotlib.pyplot (https://matplotlib.org/stable/api/_as_gen/matplotlib.pyplot.imshow.html). Priložite sliki
-    # interpoliranih območij, pridobljenih z interpolacijo ničtega oz. prvega reda interpolacijskega območja, ki sta v dimenzijskem sorazmerju z interpolacijskim območjem. Priložite
+    # Pri prikazovanju slik vzpostavite dimenzijsko sorazmerje med interpolacijskim in 
+    # interpoliranim območjem (tj. območji morata biti enakih fizičnih dimenzij), in sicer tako, 
+    # da spremenite obstoječo funkcijo displayImage z dodatnima vhodnima argumentom iGridX in iGridY, 
+    # ki predstavljata vektorja položajev slikovnih elementov v x in y smeri, ki
+    # omogočata izračun parametra extent v funkciji imshow iz modula matplotlib.pyplot 
+    # (https://matplotlib.org/stable/api/_as_gen/matplotlib.pyplot.imshow.html). Priložite sliki
+    # interpoliranih območij, pridobljenih z interpolacijo ničtega oz. prvega reda interpolacijskega 
+    # območja, ki sta v dimenzijskem sorazmerju z interpolacijskim območjem. Priložite
     # tudi programsko kodo spremenjene funkcije displayImage.
-
+    displayImage(image_interpolate, "Slika interpolacijskega območja")
+    displayImage(image_interpolate_zero, "Interpolacija ničtega reda", iGridX=(0,image_interpolate.shape[1]), iGridY=(image_interpolate.shape[0],0))
+    displayImage(image_interpolate_one, "Interpolacija prvega reda", iGridX=(0,image_interpolate.shape[1]), iGridY=(image_interpolate.shape[0],0))
