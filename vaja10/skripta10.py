3e5a9634039176f92c29290fab4cef84ee22d4c1
Jan <janpipan@gmail.com>
2022-12-21 22:41:05 +0100

    vaja 10 80%

--- vaja10/skripta10.py
+++ vaja10/skripta10.py
@@ -99,7 +99,7 @@ if __name__ == '__main__':
     G = computeDFT2(image,'forward')
     gR = computeDFT2(G, 'inverse')
 
-    displayImage(gR.real,"reconstructed")
+    """ displayImage(gR.real,"reconstructed")
 
 
 
@@ -116,11 +116,6 @@ if __name__ == '__main__':
 
     gf = computeDFT2(Gf, iDir='inverse')
     analyzeDFT2(gf, iOperations=['amplitude','display'], iTitle='filtered spectrum')
-
-
-
-
-
 
 
     butter16 = getFilterSpectrum(G, min(G.shape)/16, 'BHPF')
@@ -142,25 +137,54 @@ if __name__ == '__main__':
 
     Gf3 = G * analyzeDFT2(butter3, iOperations=['center'])
     gf3 = computeDFT2(Gf3, iDir='inverse')
-    displayImage(gf3.real, 'image')
+    displayImage(gf3.real, 'image') """
 
-    butter16 = getFilterSpectrum(G, min(G.shape)/16, 'BLPF')
-    analyzeDFT2(butter16, iOperations=['scale','display'], iTitle='filter spectrum')
+    BL16 = getFilterSpectrum(G, min(G.shape)/16, 'BLPF')
+    analyzeDFT2(BL16, iOperations=['scale','display'], iTitle='filter spectrum')
+    BL4 = getFilterSpectrum(G, min(G.shape)/4, 'BLPF')
+    analyzeDFT2(BL4, iOperations=['scale','display'], iTitle='filter spectrum')
+    BL3 = getFilterSpectrum(G, min(G.shape)/3, 'BLPF')
+    analyzeDFT2(BL3, iOperations=['scale','display'], iTitle='filter spectrum')
+    BH16 = getFilterSpectrum(G, min(G.shape)/16, 'BHPF')
+    analyzeDFT2(BH16, iOperations=['scale','display'], iTitle='filter spectrum')
+    BH4 = getFilterSpectrum(G, min(G.shape)/4, 'BHPF')
+    analyzeDFT2(BH4, iOperations=['scale','display'], iTitle='filter spectrum')
+    BH3 = getFilterSpectrum(G, min(G.shape)/3, 'BHPF')
+    analyzeDFT2(BH3, iOperations=['scale','display'], iTitle='filter spectrum')
 
-    Gf16 = G * analyzeDFT2(butter16, iOperations=['center'])
-    gf16 = computeDFT2(Gf16, iDir='inverse')
-    displayImage(gf16.real, 'image')
+    GfL16 = G * analyzeDFT2(BL16, iOperations=['center'])
+    gfL16 = computeDFT2(GfL16, iDir='inverse')
+    analyzeDFT2(gfL16, iOperations=['amplitude','display'], iTitle='filter spectrum')
 
-    butter4 = getFilterSpectrum(G, min(G.shape)/4, 'BLPF')
-    analyzeDFT2(butter4, iOperations=['scale','display'], iTitle='filter spectrum')
+    GfL4 = G * analyzeDFT2(BL4, iOperations=['center'])
+    gfL4 = computeDFT2(GfL4, iDir='inverse')
+    analyzeDFT2(gfL4, iOperations=['amplitude','display'], iTitle='filter spectrum')
 
-    Gf4 = G * analyzeDFT2(butter4, iOperations=['center'])
-    gf4 = computeDFT2(Gf4, iDir='inverse')
-    displayImage(gf4.real, 'image')
+    GfL3 = G * analyzeDFT2(BL3, iOperations=['center'])
+    gfL3 = computeDFT2(GfL3, iDir='inverse')
+    analyzeDFT2(gfL3, iOperations=['amplitude','display'], iTitle='filter spectrum')
 
-    butter3 = getFilterSpectrum(G, min(G.shape)/3, 'BLPF')
-    analyzeDFT2(butter3, iOperations=['scale','display'], iTitle='filter spectrum')
+    GfH16 = G * analyzeDFT2(BH16, iOperations=['center'])
+    gfH16 = computeDFT2(GfH16, iDir='inverse')
+    analyzeDFT2(gfH16, iOperations=['amplitude','display'], iTitle='filter spectrum')
 
-    Gf3 = G * analyzeDFT2(butter3, iOperations=['center'])
-    gf3 = computeDFT2(Gf3, iDir='inverse')
-    displayImage(gf3.real, 'image')
+    GfH4 = G * analyzeDFT2(BH4, iOperations=['center'])
+    gfH4 = computeDFT2(GfH4, iDir='inverse')
+    analyzeDFT2(gfH4, iOperations=['amplitude','display'], iTitle='filter spectrum')
+
+    GfH3 = G * analyzeDFT2(BH3, iOperations=['center'])
+    gfH3 = computeDFT2(GfH3, iDir='inverse')
+    analyzeDFT2(gfH3, iOperations=['amplitude','display'], iTitle='filter spectrum')
+
+
+    pumpkin_image = loadImage('vaja10/data/pumpkin-200x152-08bit.raw', [200,152], np.uint8)
+    cameraman_image = loadImage('vaja10/data/cameraman-256x256-08bit.raw', [256,256], np.uint8)
+    
+
+    pumpkinDFT = computeDFT2(pumpkin_image, 'forward')
+    cameramanDFT = computeDFT2(cameraman_image, 'forward')
+    print(f"Povprečna vrednost pattern: {np.average(image)}")
+
+    print(f"Povprečna vrednost pumpkin: {np.average(pumpkin_image)}")
+
+    print(f"Povprečna vrednost cameraman: {np.average(cameraman_image)}")
