87c31085753b24cb021659ebc2d23754bc8ccf0c
Jan <janpipan@gmail.com>
2022-12-26 13:39:29 +0100

    vaja 10 finsihed

--- vaja10/skripta10.py
+++ vaja10/skripta10.py
@@ -139,42 +139,63 @@ if __name__ == '__main__':
     gf3 = computeDFT2(Gf3, iDir='inverse')
     displayImage(gf3.real, 'image') """
 
+    # 2. Naloga
+    # Filtrirajte sliko iz navodil v frekvenčni domeni z BLPF in BHPF pri enakih mejnih frekvencah, kot so podane pod točko 4 v navodilih.
+    # Priložite izvedeno programsko kodo, izrise 2D spektrov filtra ter izrise rekonstruiranih slik po filtriranju. Kaj opazite v primerjavai s filtriranjem z ILPF oz. IHPF? Obrazložite odgovor.
+
     BL16 = getFilterSpectrum(G, min(G.shape)/16, 'BLPF')
-    analyzeDFT2(BL16, iOperations=['scale','display'], iTitle='filter spectrum')
+    analyzeDFT2(BL16, iOperations=['scale','display'], iTitle='BLPF16 filter')
     BL4 = getFilterSpectrum(G, min(G.shape)/4, 'BLPF')
-    analyzeDFT2(BL4, iOperations=['scale','display'], iTitle='filter spectrum')
+    analyzeDFT2(BL4, iOperations=['scale','display'], iTitle='BLPF4 filter')
     BL3 = getFilterSpectrum(G, min(G.shape)/3, 'BLPF')
-    analyzeDFT2(BL3, iOperations=['scale','display'], iTitle='filter spectrum')
+    analyzeDFT2(BL3, iOperations=['scale','display'], iTitle='BLPF3 filter')
     BH16 = getFilterSpectrum(G, min(G.shape)/16, 'BHPF')
-    analyzeDFT2(BH16, iOperations=['scale','display'], iTitle='filter spectrum')
+    analyzeDFT2(BH16, iOperations=['scale','display'], iTitle='BHPF16 filter')
     BH4 = getFilterSpectrum(G, min(G.shape)/4, 'BHPF')
-    analyzeDFT2(BH4, iOperations=['scale','display'], iTitle='filter spectrum')
+    analyzeDFT2(BH4, iOperations=['scale','display'], iTitle='BHPF4 filter')
     BH3 = getFilterSpectrum(G, min(G.shape)/3, 'BHPF')
-    analyzeDFT2(BH3, iOperations=['scale','display'], iTitle='filter spectrum')
+    analyzeDFT2(BH3, iOperations=['scale','display'], iTitle='BHPF3 filter')
 
     GfL16 = G * analyzeDFT2(BL16, iOperations=['center'])
     gfL16 = computeDFT2(GfL16, iDir='inverse')
-    analyzeDFT2(gfL16, iOperations=['amplitude','display'], iTitle='filter spectrum')
-
+    analyzeDFT2(gfL16, iOperations=['amplitude','display'], iTitle='Filtrirana slika z BLPF16')
+    
     GfL4 = G * analyzeDFT2(BL4, iOperations=['center'])
     gfL4 = computeDFT2(GfL4, iDir='inverse')
-    analyzeDFT2(gfL4, iOperations=['amplitude','display'], iTitle='filter spectrum')
-
+    analyzeDFT2(gfL4, iOperations=['amplitude','display'], iTitle='Filtrirana slika z BLPF4')
+    
     GfL3 = G * analyzeDFT2(BL3, iOperations=['center'])
     gfL3 = computeDFT2(GfL3, iDir='inverse')
-    analyzeDFT2(gfL3, iOperations=['amplitude','display'], iTitle='filter spectrum')
-
+    analyzeDFT2(gfL3, iOperations=['amplitude','display'], iTitle='Filtrirana slika z BLPF3')
+    
     GfH16 = G * analyzeDFT2(BH16, iOperations=['center'])
     gfH16 = computeDFT2(GfH16, iDir='inverse')
-    analyzeDFT2(gfH16, iOperations=['amplitude','display'], iTitle='filter spectrum')
-
+    analyzeDFT2(gfH16, iOperations=['amplitude','display'], iTitle='Filtrirana slika z BHPF16')
+    
     GfH4 = G * analyzeDFT2(BH4, iOperations=['center'])
     gfH4 = computeDFT2(GfH4, iDir='inverse')
-    analyzeDFT2(gfH4, iOperations=['amplitude','display'], iTitle='filter spectrum')
-
+    analyzeDFT2(gfH4, iOperations=['amplitude','display'], iTitle='Filtrirana slika z BHPF4')
+    
     GfH3 = G * analyzeDFT2(BH3, iOperations=['center'])
     gfH3 = computeDFT2(GfH3, iDir='inverse')
-    analyzeDFT2(gfH3, iOperations=['amplitude','display'], iTitle='filter spectrum')
+    analyzeDFT2(gfH3, iOperations=['amplitude','display'], iTitle='Filtrirana slika z BHPF3')
+
+    H_4_ILPF = getFilterSpectrum(G, min(G.shape)/4, 'ILPF')
+    analyzeDFT2(H_4_ILPF, iOperations=['scale','display'], iTitle='ILPF filter')
+    GfILPF = G * analyzeDFT2(H_4_ILPF, iOperations=['center'])
+    gfILPF = computeDFT2(GfILPF, iDir='inverse')
+    analyzeDFT2(gfILPF, iOperations=['amplitude','display'], iTitle='Filtrirana slika z ILPF4')
+
+    H_4_IHPF = getFilterSpectrum(G, min(G.shape)/4, 'IHPF')
+    analyzeDFT2(H_4_IHPF, iOperations=['scale','display'], iTitle='ILPF filter')
+    GfIHPF = G * analyzeDFT2(H_4_IHPF, iOperations=['center'])
+    gfIHPF = computeDFT2(GfIHPF, iDir='inverse')
+    analyzeDFT2(gfIHPF, iOperations=['amplitude','display'], iTitle='Filtrirana slika z IHPF4')
+
+    print("Pri filtriranju z Butterworthovim filtrom dobimo bolj gladko sliko, oz. pri filtriranju z idealnim filtrom dobimo več artefaktov (zveneja). To je posledica ostrega prehoda v frekvenčnem spektru, zaradi katerega v časovnem prostoru nastane zvenenje.")
+    
+    # 3. Naloga
+    # Na podlagih teh treh slik pokažite, da enosmerna komponenta slike vsebuje informacijo o povprečni sivinski vrednosti slike. Obrazložite odgovor ter priložite ustrezno programsko kodo in morebitne izrise.
 
 
     pumpkin_image = loadImage('vaja10/data/pumpkin-200x152-08bit.raw', [200,152], np.uint8)
@@ -183,8 +204,12 @@ if __name__ == '__main__':
 
     pumpkinDFT = computeDFT2(pumpkin_image, 'forward')
     cameramanDFT = computeDFT2(cameraman_image, 'forward')
+    
     print(f"Povprečna vrednost pattern: {np.average(image)}")
+    print(f"Enosmerna komponenta pattern: {G[0, 0]/np.sqrt(image.shape[0]*image.shape[1])}")
 
     print(f"Povprečna vrednost pumpkin: {np.average(pumpkin_image)}")
+    print(f"Enosmerna komponenta pumpkin: {pumpkinDFT[0, 0]/np.sqrt(pumpkin_image.shape[0]*pumpkin_image.shape[1])}")
 
     print(f"Povprečna vrednost cameraman: {np.average(cameraman_image)}")
+    print(f"Enosmerna komponenta cameraman: {cameramanDFT[0, 0]/np.sqrt(cameraman_image.shape[0]*cameraman_image.shape[1])}")
